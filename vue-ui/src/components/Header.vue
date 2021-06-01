35ba8a5ffa77b46b405e83dd4cbe565098b1e0cf
phucbuinguyen <phucbui.ng@gmail.com>
2021-06-01 14:04:02 +0700

    layuot

---
+++ vue-ui/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <h1>Todolist</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: bisque;
+}
+</style>>
---
+++ vue-ui/src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Task 1</p>
+  <p>Task 2</p>
+  <p>Task 3</p>
+  <p>Task 4</p>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+};
+</script>
+
+<style>
+</style>
